5399ed830287e015e553568e6b61f0a5872fcc11
Bhushan Sharma <bhushans.sharma86@gmail.com>
2025-03-18 14:16:59 +0530

    added inital structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,13 @@ import pluginReact from "eslint-plugin-react";
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "eslint-disable-next-line react/display-name": "off"
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat.recommended
 ];
